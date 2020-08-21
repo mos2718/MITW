e84f0014512ed4995ef7e9896d0b187110feb710
Jonah <107316154@gms.tcu.edu.tw>
2020-08-21 13:19:32 +0800

    documentation

--- 2020/FHIR_QUERY/JS/http_request.js
+++ 2020/FHIR_QUERY/JS/http_request.js
@@ -1,13 +1,13 @@
         //取得網路上的資源
         function HTTPGetData(urlStr, callback_fn) {
-            console.log(urlStr);
+            // console.log(urlStr); // debugger
             var rawFile = new XMLHttpRequest();
             rawFile.open("GET", urlStr, true);
             rawFile.setRequestHeader("Content-type", "application/json+fhir");
             rawFile.onreadystatechange = function() {
                 if (rawFile.readyState === 4) {
                     let ret = rawFile.responseText;
-                    // console.log(ret);
+                    // console.log(ret); // debugger
                     callback_fn(ret);
                 }
             }
